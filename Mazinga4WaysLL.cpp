69d83c86976915ef4417c528be35405b1e24b4ad
Alifiansya <99671383+Alifiansya@users.noreply.github.com>
2022-03-26 14:49:24 +0700

    Mazinga v0.0.21
    changelog:
    (+)
    - path
    - tokenizer
    - dequeueBack Queue method

--- Mazinga4WaysLL.cpp
+++ Mazinga4WaysLL.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 using namespace std;
 
 template <typename T>
@@ -10,11 +11,9 @@ struct QNode{
 
 template <typename T>
 struct Queue{
-    QNode<T> *front, *back;
-    void init(){
-        front = back = NULL;
-    }
-
+    QNode<T> *front = NULL, *back = NULL;
+    int size = 0;
+    
     void enqueue(T data){
         QNode<T> *newNode = new QNode<T>;
         newNode -> next = NULL;
@@ -27,6 +26,7 @@ struct Queue{
             back = back -> next = newNode;
         }
         back -> data = data;
+        size++;
     }
 
     T dequeue(){
@@ -34,17 +34,54 @@ struct Queue{
             T data = front -> data;
             if(front == back){
                 delete front;
-                init();
+                front = back = NULL;
             }
             else{
                 front = front -> next;
                 delete front -> prev;
+                front -> prev = NULL;
             }
+            size--;
             return data;
         }
         else cerr << "Q kosong tidak dapat di dequeue";
+        return "\0";
+    }
+    T dequeueBack(){
+        if(!(front == NULL)){
+            T data = back -> data;
+            if(front == back){
+                delete back;
+                front = back = NULL;
+            }
+            else{
+                back = back -> prev;
+                delete back -> next;
+                back -> next = NULL;
+            }
+            size--;
+            return data;
+        }
+        else cerr << "Q kosong tidak dapat di dequeue";
+        return "\0";
+    }
+
+    void print(){
+        for(QNode<T> *p = front; p != NULL; p = p -> next){
+            cout << p -> data;
+            if(p != back) cout << " -- ";
+        }
+        cout << endl;
     }
 };
+
+void tokenize(string str, const char delim, Queue<string> &q){
+    stringstream ss(str);
+    string s;
+    while (getline(ss, s, delim)) {
+        q.enqueue(s);
+    }
+}
 
 struct Node{
     int x,y;
@@ -55,8 +92,15 @@ struct Node{
 struct Maze{
     Node *first = NULL, *last = NULL;
     int size;
+    Node *start, *finish;
     void init(){
         Node *p, *q, *r;
+        if(size == 1){
+            first = last = new Node;
+            first -> x = first -> y = 0;
+            first -> top = first -> bot = first -> right = first -> left = NULL;
+            return;
+        }
 //        cout << size;
         for(int y = 0; y < size; y++){
             for(int x = 0; x < size; x++){
@@ -104,11 +148,43 @@ struct Maze{
     }
     
     void pathInit(string path){
+        Queue<string> q, x, y;
+        tokenize(path, ' ', q);
+        int itt = 0;
+
+        for(int i = q.size; i > 0 ; i--){
+            tokenize(q.dequeue(), ',', x);
+            y.enqueue(x.dequeueBack());
+        }
+        int xcoor = stoi(x.dequeue());
+        int ycoor = stoi(y.dequeue());
+
     	for(Node *p = first; p != NULL; p = p -> bot){
     		for(Node *q = p; q != NULL; q = q -> right){
     			q -> data = "[XX]";
 			}
 		}
+        while (true)
+        {
+            for(Node *p = first; p != NULL; p = p -> bot){
+                for(Node *q = p; q != NULL; q = q -> right){
+                    if((q -> x == xcoor) && (q -> y == ycoor)){
+                        q -> data = "[OO]";
+                        if(itt == 0) start = q;
+                        if(!(x.back == NULL)){
+                            xcoor = stoi(x.dequeue());
+                            ycoor = stoi(y.dequeue());
+                            break; break;
+                        }
+                        else {
+                            finish = q;
+                            return;
+                        }
+                        itt++;
+                    }
+                }
+            }
+        }
 	}
     
     void print(){
@@ -120,26 +196,28 @@ struct Maze{
 			cout << endl;
 		}
 	}
+
+    void printCoor(){
+		for(Node *p = first; p != NULL; p = p -> bot){
+			for(Node *q = p; q != NULL; q = q -> right){
+				cout << '[' << q -> x << ',' << p -> y <<']';
+				// cout << q -> data;
+			}
+			cout << endl;
+		}
+	}
 };
 
 int main(){
-	// string path;
-    // Maze mazinga;
+	string path;
+    Maze mazinga;
     
-    // cin >> mazinga.size;
-    // cin.ignore(256, '\n');
-    // getline(cin, path);
+    cin >> mazinga.size;
+    cin.ignore(256, '\n');
+    getline(cin, path);
     
-    // mazinga.init();
-    // mazinga.pathInit(path);
-    // mazinga.print();
-
-    Queue<int> test;
-    test.init();
-    test.enqueue(1);
-    test.enqueue(2);
-    test.enqueue(3);
-    for(int i = 0; i < 3; i++){
-        cout << test.dequeue() << endl;
-    }
+    mazinga.init();
+    mazinga.pathInit(path);
+    mazinga.print();
+    mazinga.printCoor();
 }
